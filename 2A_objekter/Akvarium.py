67060f5ca35f3be096f9d8822f9c940c6dc42db7
hmauroy <hmauroy@gmail.com>
2024-01-16 09:44:31 +0100

    IT2_B: Løste oppgave 2 og startet på oppgave 3 om akvarier fra uke 3 planen.

--- 2A_objekter/Akvarium.py
+++ 2A_objekter/Akvarium.py
@@ -18,12 +18,37 @@ class Akvarium:
         # Returnere vannhøyden ved gitt volum
         return 1000 * volum / (self.l * self.b)
 
+class Fisk:
+    def __init__(self, navn) -> None:
+        self.navn = navn
+    
+    def spis(self):
+        print(f"{self.navn} liker å spise")
+
+class Hvithai(Fisk):
+    def __init__(self, navn) -> None:
+        super().__init__(navn)
+        self.vitenskapelignavn = "Carcharodon carcharias"
+        self.lengde = 4000 # cm
+        self.vekt = 2000    # kg
+        self.favorittmat = "sel"
+    
+    def hoppe(self):
+        # Returnere true/false om den fanget en sel
+        pass
+
 def main():
     # Kjører programmet som hovedprogram.
     akv1 = Akvarium(10,20,15)
-    print(f"Volum av tomt akvarium{akv1.beregnVolum():.1f}")
-    print(f"Volum av maksfylt akvarium {akv1.vannVolum():.1f}")
-    print(f"2 liter vann når opp i høyde{akv1.vannHøyde(2):.1f} cm")
+    print(f"Volum av tomt akvarium {akv1.beregnVolum():.1f} L")
+    print(f"Volum av maksfylt akvarium {akv1.vannVolum():.1f} cm^3")
+    print(f"2 liter vann når opp i høyde {akv1.vannHøyde(2):.1f} cm")
+
+    # Lager en fisk.
+    torsk = Fisk("Torsk")
+    torsk.spis()    # Kaller på metoden spis().
+    kampfisk = Fisk("Kampfisk")
+    kampfisk.spis()
 
 if __name__ == "__main__":
     # If-testen sjekker om skriptet er selve hovedprogrammet.
