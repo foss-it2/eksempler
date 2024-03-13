5da832cc299d0d19025550cf789a6b78412d6455
hmauroy <hmauroy@gmail.com>
2024-03-13 09:43:07 +0100

    IT2: Tkinter. Lagt til løsning for firkant-animasjon for øveprøve i grafikk. Spiralbevegelsen viste seg å være mye mer kronglete enn først tenkt :(

--- 4_tkinter_grafikk/tkinter_videoundervisning/innspillinger/tempCodeRunnerFile.py
+++ 4_tkinter_grafikk/tkinter_videoundervisning/innspillinger/tempCodeRunnerFile.py
@@ -1,13 +1,1 @@
-# 1) Lager en header for overskrift
-header = tk.Canvas(frame1, width=windowWidth, height=100)
-header.pack()
-overskrift = tk.Label(header, text="Animasjonsloop")
-overskrift.pack()
-
-# 2) Lager en ramme som canvas kan ligge inni
-canvas_frame = tk.Frame(window)
-canvas_frame.pack()
-
-canvas = tk.Canvas(canvas_frame, width=canvas_width,
-                   height=canvas_height, background="black")
-canvas.pack()
+window.minsize(windowWidth, windowHeight)    # Setter størrelsen.
--- 4_tkinter_grafikk/øveprøve_grafikkvurdering/firkantanimasjon_løsning.py
+++ 4_tkinter_grafikk/øveprøve_grafikkvurdering/firkantanimasjon_løsning.py
@@ -11,13 +11,14 @@ import tkinter as tk  # Import tkinter
 
 # Lag vindu
 window = tk.Tk()  # Create a window
-windowWidth = 400
-windowHeight = 400
-window.minsize(windowWidth, windowHeight)    # Setter størrelsen.
+window.title("Spiral")  # Set title
+window.geometry('400x400')
 # Lag klokke-objekt
 canvas = tk.Canvas(window, bg="#2e2e2e", width=400,
                    height=400)    # Lager et canvas-objekt
 canvas.pack()    # Legg klokken inn i vinduet
+canvas_width = 400
+canvas_height = 400
 
 
 class Firkant:
@@ -25,19 +26,18 @@ class Firkant:
         self.x = 20
         self.y = 20
         self.l = 10
-        self.fart = 10
+        self.fart = 20
         self.x_retning = 1
         self.y_retning = 0
-        self.x_spiral = 0
-        self.y_spiral = 0
         self.posisjon_x = 0
         self.posisjon_y = 0
+        self.fill = "dodgerblue"
         self.outline = "#fd6496"
 
     def tegn(self):
         canvas.create_rectangle(self.x-self.l, self.y-self.l,
                                 self.x+self.l, self.y+self.l,
-                                outline=self.outline, width=3, tags="firkant")
+                                outline=self.outline,width=3, tags="firkant")
 
     def fjern(self):
         canvas.delete("firkant")
@@ -48,9 +48,13 @@ firkant.tegn()
 
 teller = 0
 isRunning = True
-offset_x = 0 # Avstand for å lage spiral
-offset_y = 0
+offset_x = 20 # Avstand for å lage spiral
+offset_y = 20
 teller = 0
+høyreOffset = 0
+bunnOffset = 0
+venstreOffset = 0
+toppOffset = 0
 while isRunning == True:
     firkant.tegn()
     canvas.after(10)
@@ -59,25 +63,34 @@ while isRunning == True:
     firkant.x += firkant.fart * firkant.x_retning
     firkant.y += firkant.fart * firkant.y_retning
     # Høyre
-    if firkant.x+firkant.l >= windowWidth - offset_x:
+    if firkant.x+firkant.l >= canvas_width - høyreOffset and firkant.x_retning == 1:
         firkant.x_retning = 0
         firkant.y_retning = 1
-        firkant.x = windowWidth - firkant.l
+        firkant.x = canvas_width - firkant.l - høyreOffset
+        høyreOffset += offset_x
     # Bunn
-    if firkant.y+firkant.l >= windowHeight - offset_y:
+    if firkant.y+firkant.l >= canvas_height - bunnOffset and firkant.y_retning == 1:
         firkant.x_retning = -1
         firkant.y_retning = 0
-        firkant.y = windowWidth - firkant.l
+        firkant.y = canvas_height - firkant.l - bunnOffset
+        bunnOffset += offset_y
     # Venstre
-    if firkant.x-firkant.l <= 0 + offset_x:
+    if firkant.x-firkant.l <= venstreOffset and firkant.x_retning == -1:
         firkant.x_retning = 0
         firkant.y_retning = -1
-        firkant.x = firkant.l
+        firkant.x = firkant.l + venstreOffset
+        venstreOffset += offset_x
     # Topp
-    if firkant.y-firkant.l <= 0 + offset_y:
+    if firkant.y-firkant.l <= toppOffset and firkant.y_retning == -1:
         firkant.x_retning = 1
         firkant.y_retning = 0
-        firkant.y = firkant.l
+        firkant.y = firkant.l + toppOffset
+        toppOffset += offset_y
+    # Sjekker om firkanten har nådd omtrentlig sentrum
+    print(f"høyreOffset,toppOffset {høyreOffset},{toppOffset}")
+    if 0.98*høyreOffset > canvas_width/2:
+        isRunning = False
+        firkant.tegn()
 
     teller += 1
     if teller >= 10000000:
